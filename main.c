66a2d39de9640e293ac064fcf1ba850720fb0f95
Hui Yu <ityuhui@gmail.com>
2021-03-04 19:55:26 +0800

    update code style

--- main.c
+++ main.c
@@ -4,26 +4,24 @@
 #define MAX_SLOT_NUMBER 10
 
 typedef struct base_t {
-    struct base_vtbl_t *vptr ;
+    struct base_vtbl_t *vptr;
 } base;
 
 typedef struct base_vtbl_t {
-    void (*fun_1)(base *);
+    void (*fun_1) (base *);
 } base_vtbl;
 
 typedef struct derived_vtbl_t {
-    void (*fun_1)(base *);
+    void (*fun_1) (base *);
 } derived_vtbl;
 
-
-
-void base_init(base *p) 
+void base_init(base * p)
 {
     char fname[] = "base_init";
     printf("%s\n", fname);
 }
 
-void base_virtual_fun_1(base *p)
+void base_virtual_fun_1(base * p)
 {
     char fname[] = "base_virtual_fun_1";
     printf("%s\n", fname);
@@ -39,12 +37,11 @@ void derived_init()
     printf("%s\n", fname);
 }
 
-void derived_virtual_fun_1(derived *p)
+void derived_virtual_fun_1(derived * p)
 {
     char fname[] = "derived_virtual_fun_1";
     printf("%s\n", fname);
 }
-
 
 int main()
 {
@@ -54,10 +51,10 @@ int main()
     derived_vtbl dvtbl;
     dvtbl.fun_1 = derived_virtual_fun_1;
 
-    base *pb = (base *)calloc(1, sizeof(base));
+    base *pb = (base *) calloc(1, sizeof(base));
     pb->vptr = &bvtbl;
 
-    base *pd = (base *)calloc(1, sizeof(derived));
+    base *pd = (base *) calloc(1, sizeof(derived));
     pd->vptr = &dvtbl;
 
     pb->vptr->fun_1(pb);
